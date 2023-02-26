85ae55eca366db5094f067a209c12066b778223b
Peter Elst <peter.elst@gmail.com>
2023-02-26 00:09:39 +0000

    initial screenshot prototype

---
+++ screenshot.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/chromedp/chromedp"
+	"github.com/pkg/browser"
+)
+
+func main() {
+	url := flag.String("url", "https://www.golang.org", "URL of the webpage to capture")
+	file := flag.String("file", "screenshot.png", "File name of the capture")
+	open := flag.Bool("open", false, "Open the image when captured")
+	flag.Parse()
+
+	var buf []byte
+	buf, err := screenshot(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writeFile(file, buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *open {
+		browser.OpenFile(*file)
+	}
+}
+
+func screenshot(url *string) ([]byte, error) {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var buf []byte
+	err := chromedp.Run(ctx, chromedp.Tasks{
+		chromedp.Navigate(*url),
+		chromedp.CaptureScreenshot(&buf),
+	})
+	return buf, err
+}
+
+func writeFile(file *string, buf []byte) error {
+	return os.WriteFile(*file, buf, 0o644)
+}
---
+++ screenshot_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestScreenshotURLs(t *testing.T) {
+	tt := []struct {
+		url     string
+		wantErr bool
+	}{
+		{url: "https://www.golang.org", wantErr: false},
+		{url: "bbc.com", wantErr: true},
+		{url: "", wantErr: true},
+	}
+	for _, tt := range tt {
+		_, err := screenshot(&tt.url)
+
+		if err != nil && !tt.wantErr {
+			t.Fatalf("Screenshot URL is invalid: '%s'", tt.url)
+		}
+	}
+}
+
+func TestWriteFile(t *testing.T) {
+	tt := []struct {
+		file    string
+		wantErr bool
+	}{
+		{file: "filename.png", wantErr: false},
+		{file: "", wantErr: true},
+	}
+	for _, tt := range tt {
+		var buf []byte
+		err := writeFile(&tt.file, buf)
+		defer os.Remove(tt.file)
+
+		if err != nil && !tt.wantErr {
+			t.Fatalf("Failed to write file: '%s'", tt.file)
+		}
+	}
+}
